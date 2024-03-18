20055c02ed6b1b7498cfcd4a880dc836b1b9a4e0
Leonardo Custodio <leonardomcustodio@hotmail.com>
2024-03-18 20:35:07 -0300

    Almost everything finished, I just have to do the add and remove item methods

--- java_course/src/main/java/br/com/leocustodio/composition/Main.java
+++ java_course/src/main/java/br/com/leocustodio/composition/Main.java
@@ -6,19 +6,20 @@ import java.time.format.DateTimeFormatter;
 import java.time.temporal.TemporalAccessor;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
         DateTimeFormatter dtf1 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         Client client;
-        OrderStatus orderStatus = OrderStatus.PENDING_PAYMENT;
         List<OrderItem> listOrderItem = new ArrayList<>();
         Order order;
 
-        System.out.println("Enter client data: ");
+        System.out.println("ENTER CLIENT DATA");
         System.out.print("Name: ");
         String name = sc.nextLine();
         System.out.print("Email: ");
@@ -38,20 +39,41 @@ public class Main {
         for (int i = 0; i < numItems; i++) {
             sc.nextLine();
             System.out.printf("Enter #%d item data %n", (i+1));
-            System.out.print("Product name: ");
-            String prodName = sc.nextLine();
-            System.out.print("Product price: ");
-            double prodPrice = sc.nextDouble();
-            sc.nextLine();
-            System.out.print("Quantity: ");
-            int prodQuantity = sc.nextInt();
-            sc.nextLine();
-            Product prod = new Product(prodName, prodPrice);
-            OrderItem orderAux = new OrderItem(prodQuantity, prod);
-            listOrderItem.add(orderAux);
+            Order.addItem();
+//            System.out.print("Product name: ");
+//            String prodName = sc.nextLine();
+//            System.out.print("Product price: ");
+//            double prodPrice = sc.nextDouble();
+//            sc.nextLine();
+//            System.out.print("Quantity: ");
+//            int prodQuantity = sc.nextInt();
+//            Product prod = new Product(prodName, prodPrice);
+//            OrderItem orderAux = new OrderItem(prodQuantity, prod);
+//            listOrderItem.add(orderAux);
         }
 
-        order = new Order(LocalDateTime.now(), orderStatus, client, listOrderItem);
+        order = new Order(LocalDateTime.now(), OrderStatus.valueOf(status), client, listOrderItem);
+        System.out.println();
+        System.out.println(order);
+
+        //Adding more items
+        System.out.println();
+        System.out.println("Do you want to add one more product (s/n): ");
+        char choice;
+        choice = sc.next().charAt(0);
+        while (choice == 's'){
+            sc.nextLine();
+            System.out.println("Enter new item data");
+            Order.addItem();
+            System.out.println("Do you want to add one more product (s/n): ");
+            choice = sc.next().charAt(0);
+        }
+
+
+
+
+        sc.close();
+        
 
 
 
--- java_course/src/main/java/br/com/leocustodio/composition/Order.java
+++ java_course/src/main/java/br/com/leocustodio/composition/Order.java
@@ -1,15 +1,21 @@
 package br.com.leocustodio.composition;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Order {
-    LocalDateTime moment;
-    OrderStatus status;
+    private LocalDateTime moment;
+    private OrderStatus status;
+    private Double totalOrder = 0.0;
 
     Client client;
-    List<OrderItem> listOrderItem = new ArrayList<>();
+    static List<OrderItem> listOrderItem = new ArrayList<>();
+
+    DateTimeFormatter dtf1 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+    DateTimeFormatter dtf2 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     public Order(){
 
@@ -19,11 +25,10 @@ public class Order {
         this.moment = moment;
         this.status = status;
         this.client = client;
-        this.listOrderItem = listOrderItem;
+        Order.listOrderItem = listOrderItem;
     }
 
-    public Order(LocalDateTime now, int value, Client client, List<OrderItem> listOrderItem) {
-    }
+    //public Order(LocalDateTime now, int value, Client client, List<OrderItem> listOrderItem) {    }
 
     public LocalDateTime getMoment() {
         return moment;
@@ -49,18 +54,41 @@ public class Order {
         this.status = status;
     }
 
-    public void addItem(){
-        OrderItem orderItem = new OrderItem();
-        orderItem.setQuantity(10);
-        //orderItem.getProduct().
-        //listOrderItem.add(orderItem);
+    public static void addItem(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Product name: ");
+        String prodName = sc.nextLine();
+        System.out.print("Product price: ");
+        double prodPrice = sc.nextDouble();
+        sc.nextLine();
+        System.out.print("Quantity: ");
+        int prodQuantity = sc.nextInt();
+        Product prod = new Product(prodName, prodPrice);
+        OrderItem orderAux = new OrderItem(prodQuantity, prod);
+        listOrderItem.add(orderAux);
+    }
+
+    public Double totalOrder(){
+        return totalOrder;
     }
 
     public String toString(){
         StringBuilder sb = new StringBuilder();
-        sb.append("ORDER SUMMARY %n");
-        sb.append("Order moment: ").append(moment).append("%n");
-
+        sb.append("ORDER SUMMARY \n");
+        sb.append("Order moment: ");
+        sb.append(dtf1.format(getMoment())).append("\n");
+        sb.append("Order status: ").append(status).append("\n");
+        sb.append("Cliente: ").append(client.getName()).append(" (").append(dtf2.format(client.getBirthDate())).append(") ").append(client.getEmail()).append("\n");
+        sb.append("Order items: ").append("\n");
+        for (OrderItem item : listOrderItem){
+            sb.append(item.getProduct().getName());
+            sb.append(", ").append(String.format("%.2f", item.getProduct().getPrice()));
+            sb.append(", Quantity: ").append(item.getQuantity());
+            sb.append(", Subtotal: $").append(String.format("%.2f", item.subTotal())).append("\n");
+            totalOrder += item.subTotal();
+        }
+        sb.append("TOTAL ORDER: $");
+        sb.append(String.format("%.2f", totalOrder));
         return sb.toString();
     }
 }
